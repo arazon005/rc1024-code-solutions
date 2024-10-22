33bdc98ae7406a2bd8fe62526cc255fca7824a3e
arazon005 <arazon005@gmail.com>
2024-10-22 23:11:41 +0000

    Completed typescript-data-model

---
+++ typescript-data-model/order-history.ts
@@ -0,0 +1,42 @@
+interface Order {
+  items: string[];
+  costs: number[];
+  deliveryDate: string[];
+  orderPlaced: string;
+  orderID: string;
+  total: number;
+}
+const orderHistory: Order[] = [
+  {
+    items: ['Javascript for impatient programmers'],
+    costs: [31.55],
+    deliveryDate: ['Aug 8, 2020'],
+    orderPlaced: 'Aug 4, 2020',
+    orderID: 'real order id here',
+    total: 31.55,
+  },
+  {
+    items: ['Timeless Way of Building'],
+    costs: [41.33],
+    deliveryDate: ['Jul 20, 2020'],
+    orderPlaced: 'Jul 19, 2020',
+    orderID: 'real order id here',
+    total: 41.33,
+  },
+  {
+    items: ['GCC Adapter'],
+    costs: [15.98],
+    deliveryDate: ['Jul 7, 2020'],
+    orderPlaced: 'Jul 4, 2020',
+    orderID: 'real order id here',
+    total: 41.33,
+  },
+  {
+    items: ['GCC - Smash Bros Edition', 'The Art of Sql'],
+    costs: [94.95, 33.99],
+    deliveryDate: ['Jul 5, 2020', 'Jul 5, 2020'],
+    orderPlaced: 'Jul 3, 2020',
+    orderID: 'real order id here',
+    total: 138.93,
+  },
+];
