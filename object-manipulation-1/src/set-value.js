0849da0ad1f230ea4a1ad120303416ec064e0149
arazon005 <arazon005@gmail.com>
2024-10-29 21:06:12 +0000

    Resubmitting object manipulation 1

--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -1,7 +1,7 @@
 'use strict';
 /* exported setValue */
 /**
- * define a function 'setValue', with three arguments 'object' of type object, 'key' of type string,
+ * define a function 'setValue', with 3 arguments 'object' of type object, 'key' of type string,
  * and 'value' of any type, which will return no value
  *  set the value contained within the property of 'key' inside 'object' to 'value'
  */
