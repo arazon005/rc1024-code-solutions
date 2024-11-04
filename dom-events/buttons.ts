b934ae3a0b80a32ee6de4dd73f6bcd6ab657e51f
arazon005 <arazon005@gmail.com>
2024-11-04 20:02:49 +0000

    completed dom-events

---
+++ dom-events/buttons.ts
@@ -0,0 +1,33 @@
+const $clickButton = document.querySelector('.click-button');
+
+if (!$clickButton) throw new Error('$clickButton does not exist');
+
+function handleClick(event: Event): void {
+  console.log('button clicked');
+  console.log(event);
+  console.log(event.target);
+}
+
+$clickButton.addEventListener('click', handleClick);
+
+const $hoverButton = document.querySelector('.hover-button');
+if (!$hoverButton) throw new Error('$hoverButton does not Exist');
+
+function handleMouseover(event: Event): void {
+  console.log('button hovered');
+  console.log(event);
+  console.log(event.target);
+}
+
+$hoverButton.addEventListener('mouseover', handleMouseover);
+
+const $doubleClick = document.querySelector('.double-click-button');
+if (!$doubleClick) throw new Error('$doubleClick does not exist');
+
+function handleDoubleClick(event: Event): void {
+  console.log('button double clicked');
+  console.log(event);
+  console.log(event.target);
+}
+
+$doubleClick.addEventListener('dblclick', handleDoubleClick);
