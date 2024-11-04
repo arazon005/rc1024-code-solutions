c419411f0c9e828533d01767f46b206d141fb6bc
arazon005 <arazon005@gmail.com>
2024-11-04 17:58:14 +0000

    completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,16 @@
+'use strict';
+const $h1 = document.querySelector('h1');
+console.log($h1);
+console.dir($h1);
+const $explanation = document.querySelector('#explanation');
+console.log($explanation);
+console.dir($explanation);
+const $hint = document.querySelector('.hint');
+console.log($hint);
+console.dir($hint);
+const $pAll = document.querySelectorAll('p');
+console.log($pAll);
+console.dir($pAll);
+const $exampleLinkAll = document.querySelectorAll('example-link');
+console.log($exampleLinkAll);
+console.dir($exampleLinkAll);
