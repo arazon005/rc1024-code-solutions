d6f1e008625707187f91d500c7da9a96efaf6348
arazon005 <arazon005@gmail.com>
2024-11-18 20:06:58 +0000

    Added the clearInterval to stop the function

--- typescript-timers/interval.js
+++ typescript-timers/interval.js
@@ -1,10 +1,11 @@
 'use strict';
 const $countdown = document.querySelector('.countdown-display');
 if (!$countdown) throw new Error('$countdown query failed');
-let timer = 4;
-setInterval(() => {
+let timer = 3;
+const countdown = setInterval(() => {
   if (timer === 0) {
     $countdown.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(countdown);
   } else {
     $countdown.textContent = String(timer);
     timer--;
--- typescript-timers/interval.ts
+++ typescript-timers/interval.ts
@@ -1,9 +1,10 @@
 const $countdown = document.querySelector('.countdown-display');
 if (!$countdown) throw new Error('$countdown query failed');
-let timer: number = 4;
-setInterval(() => {
+let timer: number = 3;
+const countdown = setInterval(() => {
   if (timer === 0) {
     $countdown.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(countdown);
   } else {
     $countdown.textContent = String(timer);
     timer--;
